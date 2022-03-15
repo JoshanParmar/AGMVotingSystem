7fec976b8357a037a12a83ec5db8e29489e775fb
Joshan Parmar <joshanparmar01@gmail.com>
2022-03-15 15:13:05 +0000

    Updated so users are not autoverified

--- register.php
+++ register.php
@@ -80,7 +80,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $param_username = $username;
             $param_realname = $realname;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            $param_verified = 1;
+            $param_verified = 0;
 
             // Attempt to execute the prepared statement
             if ($stmt->execute()) {
